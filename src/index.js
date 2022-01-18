5307c51c1e556077c08bc7e04ec20b5100338998
mayaank-rana <mayank21999@gmail.com>
2022-01-18 12:50:42 +0530

    using jsx and making components and working with props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import reactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//CSS
+import './index.css'
+
+//setup vars
+const books = [
+
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY327_FMwebp_QL65_.jpg',
+    title: "Ikigai: The Japanese secret to a long and happy life",
+    author: "Héctor García"
+  },
+  {
+    id: 2,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/51GCJ4sNgpL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: "Grandparents' Bag of Stories",
+    author: "Sudha Murty "
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SX330_BO1,204,203,200_.jpg',
+    title: "Rich Dad Poor Dad",
+    author: "Robert T. Kiyosaki"
+  }
+];
+
+// const names = ["mayank", "raj", "hari"]
+// const newNames = names.map((name) => {
+//   return <h1>{name}</h1>
+// })
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+
+        return (
+          <Book key={book.id} {...book}></Book>
+
+        );
+      })}
+
+    </section>
+  );
+}
+
+
+
+const Book = (props) => {
+  const { img, author, title } = props;
+  // console.log(props)
+  return (
+    <article className='book'>
+      <img
+        src={img}
+        alt="book img"
+      />
+      <h1>{title} </h1>
+      <h3 >{author} </h3>
+
+    </article>);
+}
+
+// const Image = () => <img src="https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY327_FMwebp_QL65_.jpg" alt="book img" />
+// const Title = () => <h1>Ikigai: The Japanese secret to a long and happy life </h1>
+// const Author = () => <h3 style={{ color: "#617d98", fontSize: "0.75rem", margin: "0.25rem" }}>Héctor García</h3>
+
+
+reactDom.render(<BookList />, document.getElementById("root"))
